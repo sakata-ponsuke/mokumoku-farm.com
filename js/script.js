03dc60cb4d0c64ed5a48cbf9c4fe5511ce1eecfb
さかた ぽんすけ <84580242+sakata-ponsuke@users.noreply.github.com>
2024-11-15 14:35:54 +0900

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// ハンバーガーメニュー
-document.getElementById("hamburger").addEventListener("click", function () {
-  document.querySelector(".sp-nav").classList.toggle("open");
-  document.body.classList.toggle("hamburger-active");
-});
-
