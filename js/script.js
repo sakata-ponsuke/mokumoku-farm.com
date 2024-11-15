ca9ade539aac3eeda7bfc1bbc85e88dd0299ab04
さかた ぽんすけ <84580242+sakata-ponsuke@users.noreply.github.com>
2024-11-15 13:03:52 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,17 +4,3 @@ document.getElementById("hamburger").addEventListener("click", function () {
   document.body.classList.toggle("hamburger-active");
 });
 
-// スライダー
-$(document).ready(function () {
-  $(".slider").slick({
-    infinite: true, // 無限ループ
-    slidesToShow: 1, // 表示するスライド数
-    slidesToScroll: 1, // スクロールするスライド数
-    autoplay: true, // 自動再生
-    autoplaySpeed: 5000, // 自動再生のスピード（ミリ秒）
-    arrows: true, // 矢印ナビゲーションを表示
-    dots: true, // ドットナビゲーションを表示
-    centerMode: true, // 画像を中央に配置
-    centerPadding: "20%", // 左右の隠れた部分の表示幅
-  });
-});
